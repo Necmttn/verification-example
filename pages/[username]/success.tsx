4002f691bbbe707108298b287b9b6db98245cc63
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2020-11-10 20:39:57 +0800

    little info

--- pages/[username]/success.tsx
+++ pages/[username]/success.tsx
@@ -18,12 +18,19 @@ const SuccessPage: React.FC = () => {
       <Confetti width={width} height={height} />
       <main className={"h-full min-h-screen bg-gray-100"}>
         <div className={"flex flex-column items-center justify-center h-full min-h-screen"}>
-          <div className={"max-w-sm shadow rounded h-full w-full p-8 flex flex-col items-center bg-white"}>
+          <div className={"max-w-lg shadow rounded h-full w-full p-8 flex flex-col items-center bg-white"}>
             <div className={"text-5xl leading-tight mx-auto"}>Success</div>
             <div className={"mt-8 px-4"}>
               <h3>
                 Welcome back <span className={"font-bold"}>{query.username}</span>
               </h3>
+            </div>
+            <div>
+              <p className={"text-gray-500 mt-10"}>
+                Base on the research confetti makes people <br />
+                simulate the emotion of wining. which reduces the <br />
+                churn rate and increases the conversion
+              </p>
             </div>
           </div>
         </div>
